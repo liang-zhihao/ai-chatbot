17091673f86eb96f550b06bbcb83bbd7e30654bc
HaoyangZheng11 <haoyzheng1@student.unimelb.edu.au>
2023-11-02 16:26:45 +1100

    update prompt box

---
+++ Aichatbot/app/src/main/java/com/unimelb/aichatbot/modules/profile/activity/InputNameBottomSheetDialogFragment.java
@@ -0,0 +1,52 @@
+package com.unimelb.aichatbot.modules.profile.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.unimelb.aichatbot.R;
+
+public class InputNameBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    private EditText input;
+    private Button confirmButton;
+    private Button cancelButton;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_input_name_bottom_sheet_dialog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        input = view.findViewById(R.id.edit_text_input_name);
+        confirmButton = view.findViewById(R.id.button_confirm);
+        cancelButton = view.findViewById(R.id.button_cancel);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newName = input.getText().toString();
+                // 在这里处理用户输入的名字
+                // 例如，保存到数据库或更新 UI 等
+
+                dismiss();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+}
--- Aichatbot/app/src/main/java/com/unimelb/aichatbot/modules/profile/activity/ProfileActivity.java
+++ Aichatbot/app/src/main/java/com/unimelb/aichatbot/modules/profile/activity/ProfileActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.TextView;
 import com.unimelb.aichatbot.R;
+
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -19,6 +21,25 @@ public class ProfileActivity extends AppCompatActivity {
             actionBar.setDisplayShowHomeEnabled(true);
         }
         setContentView(R.layout.profile_layout);
+
+        TextView nameButton = findViewById(R.id.buttonName);
+        nameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputNameBottomSheetDialogFragment bottomSheetDialogFragment = new InputNameBottomSheetDialogFragment();
+                bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
+            }
+        });
+
+        TextView buttonPassword = findViewById(R.id.buttonPassword);
+        buttonPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputPasswordBottomSheetDialogFragment bottomSheetDialogFragment = new InputPasswordBottomSheetDialogFragment();
+                bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
+            }
+        });
+
     }
 
     @Override
